f945145d7f4769e8ce399038c46c7ced234f5775
Andy Wolf <ndrwwlf@gmail.com>
2022-09-15 11:39:07 -0600

    added base.html, updated home and about templates

--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,11 @@ from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
-    template_name: "home.html"
+    template_name = "home.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
 
 
 # Create your views here.
